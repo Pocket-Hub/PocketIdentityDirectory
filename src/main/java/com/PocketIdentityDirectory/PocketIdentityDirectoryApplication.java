938927eb54f8f0f0da4d48ef8bdff14b19f2270a
Hyusein Hyuseinov <hyusein.hyuseinov@roiable.com>
2026-01-07 13:40:12 +0200

    initial commit

---
+++ src/main/java/com/PocketIdentityDirectory/PocketIdentityDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.PocketIdentityDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketIdentityDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketIdentityDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PocketIdentityDirectory/PocketIdentityDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PocketIdentityDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketIdentityDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
